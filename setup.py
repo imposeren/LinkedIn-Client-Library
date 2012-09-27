0ebd1e301166dce9482cf34295eeca49d75805ba
Yaroslav Klyuyev <imposeren@gmail.com>
2012-09-27 15:51:33 +0300

    requirments added

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='LinkedIn Client Library',
-    version='1.0',
+    version='1.0.1',
     author='Aaron Brenzel',
     author_email='abrenzel@millerresource.com',
     url='12.236.169.60:4830/liclient',
+    install_requires = ['lxml', 'httplib2'],
     description='Library for accessing the LinkedIn API in Python',
     packages=['liclient', 'liclient.parsers', 'liclient.oauth2', 'liclient.analysis'],
     license='MIT',
